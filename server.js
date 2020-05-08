eb05fbcbf5ac7f9471218e2b9e4241254d97c3cc
Michael Bowman <micbowman25@gmail.com>
2020-05-07 20:26:10 -0500

    finished homework

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ mongoose.connect("mongodb://localhost/budget", {
 
 // routes here
 
+app.use(require("./routes/api.js"));
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
