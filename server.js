d4c0b9ef9980a0b15ea2dd8e2d9bc99ac09a65f0
Michael Bowman <micbowman25@gmail.com>
2020-05-07 21:03:49 -0500

    getting it to work

--- server.js
+++ server.js
@@ -26,4 +26,10 @@ app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
+  
+  mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
 });
