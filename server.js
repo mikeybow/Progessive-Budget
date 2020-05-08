e7386c247c9b3bd655c6bde447226afe168a1f5a
Michael Bowman <micbowman25@gmail.com>
2020-05-07 21:09:03 -0500

    port

--- server.js
+++ server.js
@@ -27,9 +27,6 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
   
-  mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+  const PORT = process.env.PORT || 3000;
 
 });
